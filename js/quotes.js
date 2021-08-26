ac1f5e353312e8ffc4d483eddb31b42b2121a1f1
ergate1234 <87896754+ergate1234@users.noreply.github.com>
2021-08-26 18:20:48 +0900

    2021.08.26

--- js/quotes.js
+++ js/quotes.js
@@ -1,43 +1,43 @@
 const quotes = [
   {
     quote: "Action is the foundational key to all success.",
-    author: "Pablo Picasso"
+    author: "-Pablo Picasso-"
   },
   {
     quote: "The way get started is to quit talking and begin doing.",
-    author: "Walt Disney",
+    author: "-Walt Disney-",
   },
   {
     quote: "The greatest glory in living lies not in never falling, but in rising every time we fall.",
-    author: "Nelson Mandela",
+    author: "-Nelson Mandela-",
   },
   {
     quote: "Age is no guarantee of maturity.",
-    author: "Lawana Blackwell",
+    author: "-Lawana Blackwell-",
   },
   {
     quote: "Science is organized knowledge. Wisdom is organized life.",
-    author: "Immanuel Kant",
+    author: "-Immanuel Kant-",
   },
   {
     quote: "Life is not fair. Get used to it.",
-    author: "Bill Gates",
+    author: "-Bill Gates-",
   },
   {
     quote: "Success is walking from failure to failure with no loss of enthusiasm.",
-    author: "Winston Churchill",
+    author: "-Winston Churchill-",
   },
   {
     quote: "You can delay but you can deny.",
-    author: "Tyron Woodley",
+    author: "-Tyron Woodley-",
   },
   {
     quote: "What we dwell on is who we become.",
-    author: "Oprah Winfrey",
+    author: "-Oprah Winfrey-",
   },
   {
     quote: "It's choice - not chance - that determines your destiny.",
-    author: "Jean Nidetch",
+    author: "-Jean Nidetch-",
   }
 ]
 
